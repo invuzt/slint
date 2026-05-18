fdb600e71bbeb9fcd6a207fb056e4b7382b3f91d
invuzt <invuzt@gmail.com>
2026-05-18 00:18:19 +0000

    Fix Slint features: use backend-android-activity-05 instead of android

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,12 @@ slint::include_modules!();
 use android_activity::AndroidApp;
 
 #[no_mangle]
-fn android_main(_app: AndroidApp) {
+fn android_main(app: AndroidApp) {
     android_logger::init_once(
         android_logger::Config::default().with_max_level(log::LevelFilter::Info)
     );
+    
+    slint::platform::set_platform(Box::new(slint::platform::android::AndroidPlatform::new(app))).unwrap();
     
     let app = App::new().unwrap();
     let app_weak = app.as_weak();
